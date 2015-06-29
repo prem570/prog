e3e60be66cb8360b8953efb11aa6981240566f79
rohith.uppala <rohith.uppala@goibibo.com>
2015-06-30 00:16:22 +0530

    fibonacci code

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+def number(n):
+	result = []
+	a = 0
+	b = 1
+	result.append(a)
+	result.append(b)
+	for i in range(n-1):
+		c = a + b
+		a = b
+		b = c
+		result.append(c)
+	print result
+number(1000)
